b42f3a2f7baee20525cb6f1632ef52bb2292c13e
Yan Teixeira <yanteixeira@MacBook-Pro-de-Yan.local>
2020-10-29 23:18:08 -0300

    tratamento de erro

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
 const express = require('express')
 const app = express()
 const https = require('https')
-const data = require('./data')
-//const data = require('./data')
-
-
 
 let url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json"
 
 app.get('/pokemon', (req, res) => {
-    https.get(url, resp => {
-        let body = ''
-        resp.on('data', data => {
+    https.get(url, response => {
+        let body = ''; poke = []; result = {};
+        response.on('data', data => {
             body += data
         })
-        resp.on('end', () => {
-            var d = JSON.parse(body)
-            res.send(d.pokemon)
-            
+        response.on('end', () => {
+            var d = JSON.parse(body); var poke = []
+            for(x in d.pokemon){
+                poke.push(d.pokemon[x].name)
+            }
+            result["pokemons"] = poke
+            res.status(200).send(result) 
         }) 
     
     })
 })
 
 app.get('/pokemon/:name', (req, res) => {
-    https.get(url, resp => {
-        let body = ''; let result = "não existe"
-        resp.on('data', data => {
+    https.get(url, response => {
+        let body = ''; var result = {};
+        result["error"] = "Pokemon não encontrado."
+        response.on('data', data => {
             body += data
         })
-        resp.on('end', () => {
+        response.on('end', () => {
             var d = JSON.parse(body)
             for(x in d.pokemon){
                 if (d.pokemon[x].name.toLowerCase().valueOf() == req.params.name.toLowerCase().valueOf()){
                     result = d.pokemon[x]
                 }
             };
-            res.send(result)
+            res.status(200).send(result)
         }) 
     })
     
 })
 
 app.post('/advantage/pokemon/:name', (req, res) => {
-    https.get(url, resp => {
-        let body = ''; let aux = []; exists = false; poke = []
-        resp.on('data', data => {
+    https.get(url, response => {
+        var body = ''; var aux = [];var exists = false; var poke = []; var result = {};
+        response.on('data', data => {
             body += data
         })
-        resp.on('end', () => {
+        response.on('end', () => {
             var d = JSON.parse(body)
             for(x in d.pokemon){
                 if (d.pokemon[x].name.toLowerCase().valueOf() == req.params.name.toLowerCase().valueOf()){
@@ -63,19 +63,23 @@ app.post('/advantage/pokemon/:name', (req, res) => {
                         poke.push(d.pokemon[x].name)
                     }
                 }
-                res.send(poke)
+                result['advantage_against']=poke
+            
+            }else{
+                result['error']="Pokemon não encontrado."
             }
+            res.status(200).send(result)
         }) 
     })
 })
 
 app.post('/weakness/pokemon/:name', (req, res) => {
-    https.get(url, resp => {
-        let body = ''; let aux = []; exists = false; result = []
-        resp.on('data', data => {
+    https.get(url, response => {
+        let body = ''; let aux = []; exists = false; poke = []; result = {}
+        response.on('data', data => {
             body += data
         })
-        resp.on('end', () => {
+        response.on('end', () => {
             var d = JSON.parse(body)
             for(x in d.pokemon){
                 if (d.pokemon[x].name.toLowerCase().valueOf() == req.params.name.toLowerCase().valueOf()){
@@ -88,14 +92,15 @@ app.post('/weakness/pokemon/:name', (req, res) => {
                 for(x in d.pokemon){
                     aux = d.pokemon[x].type.filter(value => weaknesses.includes(value))
                     if (aux.length > 0){
-                        result.push(d.pokemon[x].name)
+                        poke.push(d.pokemon[x].name)
                     }
                 }
-                
+                result['weakness_against']=poke
+           
             } else{
-                result = "Pokemon não encontrado.";
+                result['error']="Pokemon não encontrado."
             }
-            res.send(result);        
+            res.status(200).send(result);        
         }) 
     })
 })
